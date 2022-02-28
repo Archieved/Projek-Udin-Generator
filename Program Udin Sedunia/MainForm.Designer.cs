c21ff796e6e9bd306c75a19a446bd308d5c3a86c
Alex Mario Simanjuntak <80665374+Archieved@users.noreply.github.com>
2022-02-28 13:46:55 +0700

    Add files via upload

---
+++ Program Udin Sedunia/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alex
+ * Date: 2/28/2022
+ * Time: 1:14 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Program_Udin_Sedunia
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(92, 199);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(200, 34);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "ADD";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label1.Location = new System.Drawing.Point(92, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(200, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Udin Name Generator";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(74, 84);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(245, 104);
+			this.listBox1.TabIndex = 2;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1SelectedIndexChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(92, 239);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(200, 34);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "REMOVE";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.textBox1.Location = new System.Drawing.Point(74, 47);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(245, 26);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(382, 285);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "Program Udin Sedunia";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
